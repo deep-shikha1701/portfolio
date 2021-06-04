1f3e3d30ea45f3dd63051caffd55b599ab166715
= <=>
2021-06-05 02:59:20 +0530

    Updated forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,6 +20,7 @@ function Contact() {
                 </div>
                 <div className="contact__content" method="POST">
                 <form name="contact_form" class="form" onSubmit="submit" netlify>
+                    <input type="hidden" name="form-name" value="contact_form" />
                     <h6>Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.
                         Simply fill the from and send me an email.</h6>
                     <p type="Name:"><input type="text" name="name" placeholder="Write your name here"></input></p>
@@ -27,13 +28,13 @@ function Contact() {
                     <p type="Message:"><textarea type="text" name="message" placeholder="What would you like to tell us"/></p>
                     <button type="Submit">Send Message</button>
                     <div className="about__social">
-                            <a href="https://www.facebook.com/deepshikha.sahu.3304/"><FacebookIcon fontSize="small" /></a>
-                            <a href="https://www.linkedin.com/in/deepshikha10/"><LinkedInIcon fontSize="small" /></a>
-                            <a href="https://twitter.com/Deepshikha_10/"><TwitterIcon fontSize="small" /></a>
-                            <a href="https://github.com/deep-shikha1701"><GitHubIcon fontSize="small" /></a>
-                            <a href="mailto:dsahu2901@gmail.com"><MailIcon fontSize="small" /></a>
-                        </div>
-                    </form>            
+                        <a href="https://www.facebook.com/deepshikha.sahu.3304/"><FacebookIcon fontSize="small" /></a>
+                        <a href="https://www.linkedin.com/in/deepshikha10/"><LinkedInIcon fontSize="small" /></a>
+                        <a href="https://twitter.com/Deepshikha_10/"><TwitterIcon fontSize="small" /></a>
+                        <a href="https://github.com/deep-shikha1701"><GitHubIcon fontSize="small" /></a>
+                        <a href="mailto:dsahu2901@gmail.com"><MailIcon fontSize="small" /></a>
+                    </div>
+                </form>            
                 </div>
             </div>            
         </div>
